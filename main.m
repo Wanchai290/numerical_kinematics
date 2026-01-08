fdc213ee612a44b15e34f7d4426ae8713384a463
Wanchai290 <wanchai.menier@gmail.com>
2026-01-08 22:37:00 +0100

    working inverse kinematics with obstacle avoidance !! :D

--- main.m
+++ main.m
@@ -37,7 +37,7 @@ RwT9_S(1:3, 4)  % expected position of end-effector
 
 
 RwPR9 = RwT9(1:3, 4);
-matlabFunction(RwPR9, 'File', 'clc_mgd.m', 'Vars', [L Theta]);
+matlabFunction(RwPR9, 'File', 'clc_mgd.m', 'Vars', {[L Theta]});
 
 %{
 % Newton's method MGI
@@ -88,7 +88,7 @@ end
 disp('end');
 %}
 
-%% Newton-Raphson + Moving inside the null-space of J
+% Newton-Raphson + Moving inside the null-space of J
 % get direct kinematic model for each rotoid joint
 RwT4 = RwT1 * R1T2 * R2T3 * R3T4;
 RwT5 = RwT1 * R1T2 * R2T3 * R3T4 * R4T5;
@@ -96,74 +96,112 @@ RwT6 = RwT1 * R1T2 * R2T3 * R3T4 * R4T5 * R5T6;
 RwT7 = RwT1 * R1T2 * R2T3 * R3T4 * R4T5 * R5T6 * R6T7;
 
 % generate function to retrieve position of each rotoid joint on the plane
-r_poses = simplify([RwT4(1:3, 4) RwT5(1:3, 4) RwT6(1:3, 4) RwT7(1:3, 4)]); % only position required
+r_poses = [RwT4(1:3, 4) RwT5(1:3, 4) RwT6(1:3, 4) RwT7(1:3, 4)]; % only position required
 
 % define obstacle to avoid
-syms xo yo zo Obs_r real;
+syms xo yo zo real;
 Obs_P = [xo;yo;zo];
 
-% L2 distance vectorized function
-function dist = clc_dist(A, B, r)
-    % size(A) = 3 1
-    d = sqrt(A - B) .^2;
-    dist(d <= r) = d;
-    dist(d > r) = 0;
+% compute distances & their gradient for each joint in 4R part
+n = length(r_poses);
+dist_q = [];
+grad_dist_q = {[]}; % ugh not very pretty
+for i = 1:n
+    dist_q = [dist_q norm(Obs_P - r_poses(:, i))];
+    grad_dist_q{i} = gradient(dist_q(i), Theta);
 end
+grad_dist_q{1}
 
-% create Phi function for vector Z
-Phi_f = 0; % scalar function
-for i = 1:length(r_poses)
-    d = clc_dist(Obs_P, r_poses(1:3, i), Obs_r);
-    if d ~= 0
-        Phi_f = Phi_f + 1 / d;
+matlabFunction(dist_q, 'File', 'clc_dist.m', 'Vars', {[L Theta Obs_P']});
+matlabFunction( ...
+    grad_dist_q{1}, grad_dist_q{2}, grad_dist_q{3}, grad_dist_q{4}, ...
+    'File', 'clc_gradient_dist.m', 'Vars', {[L Theta Obs_P']});
+
+
+function Z = clc_Z(L, q, Obs_P, Obs_r)
+    % L : lengths vector
+    % q : joint angles
+    % Obs_p : 1x3 row vector of obstacle center
+    % Obs_r : Radius of obstacle
+    
+    % get joint with closest distance to obstacle
+    dist_q = clc_dist([L q Obs_P]);
+    [v, i] = min(dist_q);
+    
+    % Check that joint is inside obstacle radius
+    if v < 2 * Obs_r
+        % we'll use this joint's gradient vector as optimization
+        % to move it away from the obstacle
+        [gdq1, gdq2, gdq3, gdq4] = clc_gradient_dist([L, q, Obs_P])
+        gdq = {gdq1 gdq2 gdq3 gdq4}
+        Z = gdq{i};
+    else
+        Z = zeros([length(q) 1]);
     end
 end
 
-% Compute the gradient of the Phi function with respect to the joint positions
-grad_Phi_f = gradient(Phi_f, [theta3 theta4 theta5 theta6 theta7 theta8])
-Z = grad_Phi_f;
-
-matlabFunction(Z, 'File', 'clc_Z.m', 'Vars', [L Theta Obs_P']);
-%%
 % define target position X
-P_target = [2; 0; 3]; % column vector
+P_target = [2.5; 0; 1]; % column vector
 P_end_effector = RwPR9;
 
 f = P_end_effector - P_target;
-matlabFunction(f, 'File', 'clc_f.m', 'Vars', [L Theta]);
+matlabFunction(f, 'File', 'clc_f.m', 'Vars', {[L Theta]});
 J_f = jacobian(f, [theta3 theta4 theta5 theta6 theta7 theta8]);
-matlabFunction(J_f, 'File', 'clc_jacobian_f.m', 'Vars', [L Theta]);
-%%
-% Newton-Raphson scheme
-clear;
+matlabFunction(J_f, 'File', 'clc_jacobian_f.m', 'Vars', {[L Theta]});
+
+%% Newton-Raphson scheme
 
 % only 4R plane robot has lengths of 1
 % and joint 3 to put it at z-height of 1
-L = [0 0 1 1 1 1 1 0];
+Lv = [0 0 1 1 1 1 1 0];
 q0 = [0; 0; 0; 0; 0; 0];
 In = eye(length(q0));
 q = q0;
 
 % obstacle O
-Obs_P = [1.5; 0 ;1];
+Obs_P = [1.5; 0; 1.5];
+Obs_r = 0.4;
 
-
-max_iter = 200;
+max_iter = 900;
 for step = 1:max_iter
     % this is messier than my own room
     % i'm open to suggestion to prettify this
-    f = clc_f(L(1), L(2), L(3), L(4), L(5), L(6), L(7), L(8), ...
-    q(1), q(2), q(3), q(4), q(5), q(6));
-    J_f = clc_jacobian_f(L(1), L(2), L(3), L(4), L(5), L(6), L(7), L(8), ...
-    q(1), q(2), q(3), q(4), q(5), q(6));
+    Z = clc_Z(Lv, q', Obs_P', Obs_r);
+
+    f = clc_f([Lv q']);
+    J_f = clc_jacobian_f([Lv q']);
     pinv_J_f = clc_pinv(J_f);
-    Z = clc_Z(L(1), L(2), L(3), L(4), L(5), L(6), L(7), L(8), ...
-        q(1), q(2), q(3), q(4), q(5), q(6), Obs_P(1), Obs_P(2), Obs_P(3))
-    
+
     q = q - pinv_J_f * f + (In - pinv_J_f * J_f) * Z;
 end
-function x = clipZeroOne(x)
-x(x < 0) = 0;
-x(x > 1) = -1;
-end
+
 q
+
+%% draw robot
+Plist = [];
+Tr = {RwT1 R1T2 R2T3 R3T4 R4T5 R5T6 R6T7 R7T8 R8T9};
+cur_tr = 1;
+for i = 1:length(Tr)
+    cur_tr = cur_tr * Tr{i};
+    P_tr = cur_tr(1:3, 4);
+    P_end_effector = subs(P_tr, [L Theta], [Lv q']);
+    Plist = [Plist P_end_effector];
+end
+double(Plist)
+
+axis equal;
+view(3);
+hold on;
+grid on;
+
+xlabel('X-axis');
+ylabel('Y-axis');
+zlabel('Z-axis');
+title('Inverse kinematics of 4R component of robot avoiding obstacle')
+plot3(Plist(1, :), Plist(2, :), Plist(3, :));
+
+[A,B,C] = sphere
+A = A * Obs_r;
+B = B * Obs_r;
+C = C * Obs_r;
+surf(A + Obs_P(1),B + Obs_P(2), C + Obs_P(3), 20, "FaceColor", "black")
