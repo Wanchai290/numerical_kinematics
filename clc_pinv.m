ccefc24531aed1c91a0cc2718580545ccce8cf7c
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2026-01-09 22:10:42 +0100

    use svd for pinv computation
    should've changed the whole file instead but nevermind

--- clc_pinv.m
+++ clc_pinv.m
@@ -1,4 +1,21 @@
 function inv = clc_pinv(X)
-% todo: SVD decomposition
-inv = pinv(X);
+% not very optimized
+% in the sense we could've changed the computations
+% in the main.m file to use SVD
+% but there was no time left
+% based on this result :
+%   https://www.johndcook.com/blog/2018/05/05/svd/
+
+[U, S, V] = svd(X);
+S_size = size(S);
+SS = zeros(S_size(2), S_size(1));
+for i = 1:S_size(1)
+    for j = 1:S_size(2)
+        x = S(i, j);
+        if x ~= 0
+            SS(j, i) = 1/x;
+        end
+    end
+end
+inv = V * SS * transpose(U);
 end
