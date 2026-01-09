73049ae5e8f98b48965ffdb50c0bbc72d4897c05
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2026-01-09 21:52:35 +0100

    3d graph and readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# Numerical Kinematics
+## How to run ?
+The file `newton_2d.m` is an exemple usage of the Newton-Raphson method for
+linear optimization.
+
+The project in itself is contained in the `main.m` file. There are in total 3
+sections of the code described below
+
+### First section : Direct geometric symbolic model
+Defines the model, as well as the target point `P_target` for the robot to attain. The condition for each joint to be at `2 * Obs_r` away from the obstacle is written at line 83.
+
+|Line|Parameter|Description|
+|-----|----------|----------------|
+| 132 |    Lv    | Length of each joint
+| 132 |    q0    | Initial solution for Newton-Raphson solver
+| 144 | P_target | Column vector of the desired position for the end-effector
+----------------------------------
+
+### Second section : Inverse geometric model solver
+Uses the Newton-Raphson method to solve the inverse geometric model of the robot, along with obstacle avoidance (if enabled by passing a parameter).
+
+|Line|Parameter|Description|
+|-----|-------|----------------|
+| 184 | Obs_P | Column vector of the obstacle's center
+| 185 | Obs_r | Obstacle radius
+--------------------------------
+
+### Third section : Resulting 3D plot
+Displays a 3D plot with both the simple solution and the solution that avoids obstacles.
--- main.m
+++ main.m
@@ -39,57 +39,8 @@ RwT9_S(1:3, 4)  % expected position of end-effector
 RwPR9 = RwT9(1:3, 4);
 matlabFunction(RwPR9, 'File', 'clc_mgd.m', 'Vars', {[L Theta]});
 
-%{
-% Newton's method MGI
-% Newton Raphson method : Solve f(x) = 0
-% We want the end-effector to be at a specific location 
-% P_target = [xp; yp; zp]
-
-% The direct geometric model is located in the last column of the 
-% homogeneous transform R1T5
-RwPR9 = RwT9(1:3, 4);
-
-% We want to have the end-effector point at a specific location
-% in the world frame
-syms xp yp zp real;
-P_target = [xp; yp; zp]; % world frame
-
-% This means solving
-%     RwPR9 = P_target
-% <=> RwPR9- P_target = 0
-f = RwPR9 - P_target
-J = jacobian(f, [theta3 theta4 theta5 theta6 theta7])
-size(J)
-% By directly computing the inverse (computationally inefficient)
-% J_inv = pinv(J)
-
-% Newton-Raphson scheme
-% Xn = Xn-1 - Jf(Xn-1)^-1 
-
-%%
-l = [0 0 1 1 1 1 1 0];
-f = subs(f, [l1 l2 l3 l4 l5 l6 l7 l8 Pi], [l pi]);
-J_inv = subs(J_inv, [l1 l2 l3 l4 l5 l6 l7 l8 Pi], [l pi]);
-
-% using values for MGD above
-S_P_target = [1, 0, 3];
-f = subs(f, [xp yp zp], S_P_target);
-J_inv = subs(J_inv, [xp yp zp], S_P_target);
-
-X = [pi; pi; pi; pi; pi];  % theta angles
-
-disp('start newton-raphson method')
-for i = 1:6
-    % replace with matlabFunction call (faster)
-    S_J_inv = subs(J_inv, [theta3 theta4 theta5 theta6 theta7], X);
-    S_f = subs(f, [theta3 theta4 theta5 theta6 theta7], X);
-    X = X - S_J_inv * S_f;
-end
-disp('end');
-%}
-
 % Newton-Raphson + Moving inside the null-space of J
-% get direct kinematic model for each rotoid joint
+% get direct kinematic model for each rotoid joint of (x, z) plane
 RwT4 = RwT1 * R1T2 * R2T3 * R3T4;
 RwT5 = RwT1 * R1T2 * R2T3 * R3T4 * R4T5;
 RwT6 = RwT1 * R1T2 * R2T3 * R3T4 * R4T5 * R5T6;
@@ -120,19 +71,19 @@ matlabFunction( ...
 
 function Z = clc_Z(L, q, Obs_P, Obs_r)
     % L : lengths vector
-    % q : joint angles
+    % q : joint angles column vector
     % Obs_p : 1x3 row vector of obstacle center
     % Obs_r : Radius of obstacle
     
     % get joint with closest distance to obstacle
-    dist_q = clc_dist([L q Obs_P]);
+    dist_q = clc_dist([L q' Obs_P]);
     [v, i] = min(dist_q);
     
     % Check that joint is inside obstacle radius
     if v < 2 * Obs_r
         % we'll use this joint's gradient vector as optimization
         % to move it away from the obstacle
-        [gdq1, gdq2, gdq3, gdq4] = clc_gradient_dist([L, q, Obs_P])
+        [gdq1, gdq2, gdq3, gdq4] = clc_gradient_dist([L q' Obs_P])
         gdq = {gdq1 gdq2 gdq3 gdq4}
         Z = gdq{i};
     else
@@ -144,6 +95,7 @@ end
 P_target = [2.5; 0; 1]; % column vector
 P_end_effector = RwPR9;
 
+% define function to solve and its jacobian
 f = P_end_effector - P_target;
 matlabFunction(f, 'File', 'clc_f.m', 'Vars', {[L Theta]});
 J_f = jacobian(f, [theta3 theta4 theta5 theta6 theta7 theta8]);
@@ -151,44 +103,67 @@ matlabFunction(J_f, 'File', 'clc_jacobian_f.m', 'Vars', {[L Theta]});
 
 %% Newton-Raphson scheme
 
+function q_out = clc_q(Lv, q0, Obs_P, Obs_r, avoid_obstacle)
+    % avoid_obstacle : bool
+    % q: column vector
+    % Obs_P: [x y z] column vector
+    % Obs_r: obstacle radius
+    In = eye(length(q0));
+    q = q0;
+    max_iter = 900;
+    for step = 1:max_iter
+        Z = clc_Z(Lv, q, Obs_P', Obs_r);
+
+        f = clc_f([Lv q']);
+        J_f = clc_jacobian_f([Lv q']);
+        pinv_J_f = clc_pinv(J_f);
+        
+        if avoid_obstacle
+            q = q - pinv_J_f * f + (In - pinv_J_f * J_f) * Z;
+        else
+            q = q - pinv_J_f * f;
+        end
+    end
+    q_out = q;
+end
+
 % only 4R plane robot has lengths of 1
 % and joint 3 to put it at z-height of 1
 Lv = [0 0 1 1 1 1 1 0];
 q0 = [0; 0; 0; 0; 0; 0];
-In = eye(length(q0));
-q = q0;
 
 % obstacle O
 Obs_P = [1.5; 0; 1.5];
 Obs_r = 0.4;
 
-max_iter = 900;
-for step = 1:max_iter
-    % this is messier than my own room
-    % i'm open to suggestion to prettify this
-    Z = clc_Z(Lv, q', Obs_P', Obs_r);
-
-    f = clc_f([Lv q']);
-    J_f = clc_jacobian_f([Lv q']);
-    pinv_J_f = clc_pinv(J_f);
-
-    q = q - pinv_J_f * f + (In - pinv_J_f * J_f) * Z;
-end
-
-q
+q = clc_q(Lv, q0, Obs_P, Obs_r, false)
+q_obs = clc_q(Lv, q0, Obs_P, Obs_r, true)
 
 %% draw robot
-Plist = [];
 Tr = {RwT1 R1T2 R2T3 R3T4 R4T5 R5T6 R6T7 R7T8 R8T9};
-cur_tr = 1;
-for i = 1:length(Tr)
-    cur_tr = cur_tr * Tr{i};
-    P_tr = cur_tr(1:3, 4);
-    P_end_effector = subs(P_tr, [L Theta], [Lv q']);
-    Plist = [Plist P_end_effector];
-end
-double(Plist)
 
+function out = robot_poses(Tr, L, Theta, Lv, q)
+    % q: column vector
+    % L: symbolic lengths variables
+    % Theta: symbolic lengths variables
+    % Lv: line vector of all lengths
+    Plist = [];
+    cur_tr = 1;
+    for i = 1:length(Tr)
+        cur_tr = cur_tr * Tr{i};
+        P_tr = cur_tr(1:3, 4);
+        P_end_effector = subs(P_tr, [L Theta], [Lv q']);
+        Plist = [Plist P_end_effector];
+    end
+    out = Plist;
+end
+
+Plist_normal = robot_poses(Tr, L, Theta, Lv, q);
+Plist_obstacle = robot_poses(Tr, L, Theta, Lv, q_obs);
+
+
+
+% draw robot
 axis equal;
 view(3);
 hold on;
@@ -198,10 +173,12 @@ xlabel('X-axis');
 ylabel('Y-axis');
 zlabel('Z-axis');
 title('Inverse kinematics of 4R component of robot avoiding obstacle')
-plot3(Plist(1, :), Plist(2, :), Plist(3, :));
+plot3(Plist_normal(1, :), Plist_normal(2, :), Plist_normal(3, :), 'DisplayName', 'Inv. Kinematics');
+plot3(Plist_obstacle(1, :), Plist_obstacle(2, :), Plist_obstacle(3, :), 'DisplayName', 'Inv. Kinematics with null-space movement (or policy)');
 
-[A,B,C] = sphere
+[A,B,C] = sphere;
 A = A * Obs_r;
 B = B * Obs_r;
 C = C * Obs_r;
-surf(A + Obs_P(1),B + Obs_P(2), C + Obs_P(3), 20, "FaceColor", "black")
+surf(A + Obs_P(1),B + Obs_P(2), C + Obs_P(3), 20, "FaceColor", "black", 'DisplayName', 'Obstacle')
+legend()
